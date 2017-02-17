95f39c162308a906cc40a0cdeaed52972e95f5ee
Young <mcpjjh@gmail.com>
2017-02-17 18:06:38 +0000

    added reviews form with description (text box) and dropdown for ratings on products pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show] do
     resources :reviews, only: [:create]
   end
+
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
