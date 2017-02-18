a7f14eda770b9db418d006d544dbbbc5dc3d6194
Young <mcpjjh@gmail.com>
2017-02-18 15:44:08 +0000

    edited css on login and register pages to match other pages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,26 @@
 class ReviewsController < ApplicationController
+
   def create
-      @product = Product.find params[:product_id]
+    @product = Product.find params[:product_id]
     @review = Review.new(review_params)
     @review.product = @product
     @review.user = current_user
     if @review.save
       redirect_to "/products/#{@product.id}"
     else
-      render plain: "Review not saved"
+      render plain: "Review not saved."
+      end
+  end
+
+  def destroy
+    @product = Product.find params[:product_id]
+    @review = Review.find params[:id]
+    @review.product = @product
+    @review.user = current_user
+    if @review.delete
+      redirect_to "/products/#{@product.id}"
+    else
+      render plain: "Not deleted."
       end
   end
 
